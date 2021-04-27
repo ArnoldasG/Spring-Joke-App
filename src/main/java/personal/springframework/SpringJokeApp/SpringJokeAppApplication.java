3a5a20376b98d9170351008de86a9a6a4e31cec7
ArnoldasG <grevasarnoldas@gmail.com>
2021-04-27 17:36:58 +0300

    Created Starter project. #1

---
+++ src/main/java/personal/springframework/SpringJokeApp/SpringJokeAppApplication.java
@@ -0,0 +1,13 @@
+package personal.springframework.SpringJokeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokeAppApplication.class, args);
+	}
+
+}
